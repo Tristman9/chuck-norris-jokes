76e5780edc16f6b170ebca57f56026b115cf056b
Tristan Dobos <tristandobos@gmail.com>
2020-09-27 09:57:29 +0200

    Updates

---
+++ precache-manifest.1fc51a9d73e790cc6fc2831e59e352af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352f49fd3f406777a527fd8f72fd6914",
+    "url": "/chuck-norris-jokes/index.html"
+  },
+  {
+    "revision": "a3651c3afffb7e126f37",
+    "url": "/chuck-norris-jokes/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d64ad4a0539cefdc378f",
+    "url": "/chuck-norris-jokes/static/css/main.e9279e84.chunk.css"
+  },
+  {
+    "revision": "a3651c3afffb7e126f37",
+    "url": "/chuck-norris-jokes/static/js/2.b9daffd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris-jokes/static/js/2.b9daffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64ad4a0539cefdc378f",
+    "url": "/chuck-norris-jokes/static/js/main.53aa1696.chunk.js"
+  },
+  {
+    "revision": "7bc61de2e57eb5907032",
+    "url": "/chuck-norris-jokes/static/js/runtime-main.52c3dbf6.js"
+  }
+]);
